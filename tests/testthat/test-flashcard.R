9510bb366d5a57b5e966c94c46557a7a0e194f91
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2022-05-20 16:55:48 -0500

    Copy operators.csv to testthat directory for tests

--- tests/testthat/test-flashcard.R
+++ tests/testthat/test-flashcard.R
@@ -1,9 +1,11 @@
 test_that("checks pass", {
-  testdeck <- read.csv("../../data/operators.csv")
+  testdeck <- read.csv("operators.csv")
   testdeck2 <- testdeck
   names(testdeck2) <- NULL
-  expect_error(flashcard("../../data/operators.csv"))
+  expect_error(flashcard("operators.csv"))
   expect_error(flashcard(testdeck2))
-  suppressMessages(expect_message(flashcard(file = "../../data/operators2.csv", package = FALSE)))
-  suppressMessages(expect_error(flashcard(file = "../../data/operators2.csv", package = TRUE)))
+  testdeck3 <- testdeck[, 1:2]
+  withr::local_file(write.csv(testdeck3, "testdeck3.csv"))
+  suppressMessages(expect_message(flashcard(file = "testdeck3.csv", package = FALSE)))
+  suppressMessages(expect_error(flashcard(file = "testdeck3.csv", package = TRUE)))
 })
