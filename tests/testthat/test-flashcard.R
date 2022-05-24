71edeb5fe32de17d7ae7e48310e17a97d28f98cd
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2022-05-24 17:23:24 -0500

    Move test data to testdata/ and use test_path()

--- tests/testthat/test-flashcard.R
+++ tests/testthat/test-flashcard.R
@@ -1,5 +1,5 @@
 test_that("validations pass", {
-  testdeck <- read.csv("operators.csv")
+  testdeck <- read.csv(test_path("testdata", "operators.csv"))
   testdeck2 <- testdeck
   names(testdeck2) <- NULL
   expect_error(validate_deck(letters))
@@ -7,11 +7,11 @@ test_that("validations pass", {
   expect_error(validate_deck(1:5))
   expect_error(validate_deck(testdeck2))
   testdeck3 <- testdeck[, 1:2]
-  write.csv(testdeck3, "testdeck3.csv")
-  suppressMessages(expect_message(validate_deck("testdeck3.csv", package = FALSE)))
+  write.csv(testdeck3, test_path("testdata", "testdeck3.csv"))
+  suppressMessages(expect_message(validate_deck(test_path("testdata", "testdeck3.csv"), package = FALSE)))
   testdeck4 <- testdeck[, 1]
-  write.csv(testdeck4, "testdeck4.csv")
-  suppressMessages(expect_error(validate_deck("testdeck4.csv", package = FALSE)))
-  file.remove(test_path("testdeck3.csv"))
-  file.remove(test_path("testdeck4.csv"))
+  write.csv(testdeck4, test_path("testdata", "testdeck4.csv"))
+  suppressMessages(expect_error(validate_deck(test_path("testdata", "testdeck4.csv"), package = FALSE)))
+  file.remove(test_path("testdata", "testdeck3.csv"))
+  file.remove(test_path("testdata", "testdeck4.csv"))
 })
