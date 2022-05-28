1595f2ca6e791dadb61b64020faaa1c340ccdabf
Jeffrey Stevens <jeffrey.r.stevens@protonmail.com>
2022-05-27 21:53:51 -0500

    Add documentation for list_decks() functions

--- R/list_decks.R
+++ R/list_decks.R
@@ -1,11 +1,33 @@
+
+#' List available built-in flashcard decks
+#'
+#' @param quiet Logical to prevent list information from printing to console.
+#'
+#' @return
+#' Outputs a list of available built-in flashcard decks to the console.
+#' @export
+#'
+#' @examples
+#' list_decks()
 list_decks <- function(quiet = FALSE) {
-  get_decks()
+  available_decks <- get_decks()
   if (!quiet) {
     cli::cli_text("Available flashcard decks")
-    cli::cli_ol(decks)
+    cli::cli_ol(available_decks$decks)
   }
 }
 
+#' Choose from available flashcard decks
+#'
+#' @param x Name of deck to directly generate flashcards (allows choose_deck()
+#' to act as wrapper for flashcard()).
+#'
+#' @return
+#' Outputs a list of available built-in flashcard decks to the console, where
+#' the user can choose of the decks to generate flashcards.
+#' @export
+#'
+#' @examples
 choose_deck <- function(x = NULL) {
   if (!is.null(x)) {
     flashcard(x)
@@ -30,7 +52,7 @@ get_decks <- function() {
   decklabels <- gsub(".csv", "", deckfiles)
   titles <- vapply(deckpaths, get_title, character(1))
   decks <- paste0(titles, " (", decklabels, ")")
-  invisible(list(decklabels = decklabels, decktitles = unname(titles)))
+  invisible(list(decklabels = decklabels, decktitles = unname(titles), decks = decks))
 }
 
 get_title <- function(x) {
