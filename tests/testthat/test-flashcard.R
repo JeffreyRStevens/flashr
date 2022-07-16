f1a896722876445ee925486c34510af5d16c8e4f
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2022-07-16 17:05:29 -0500

    Warn rather than abort when package == FALSE (and address lintr issues)

--- tests/testthat/test-flashcard.R
+++ tests/testthat/test-flashcard.R
@@ -9,7 +9,7 @@ test_that("validations pass", {
   testdeck3 <- testdeck[, 1:3]
   write.csv(testdeck3, test_path("testdata", "testdeck3.csv"))
   expect_message(validate_deck(test_path("testdata", "testdeck3.csv"),
-                               package = FALSE
+    package = FALSE
   ), "so using filename for title")
   suppressMessages(expect_message(validate_deck(
     test_path("testdata", "testdeck3.csv"),
@@ -18,19 +18,19 @@ test_that("validations pass", {
   testdeck4 <- testdeck[, 1:2]
   write.csv(testdeck4, test_path("testdata", "testdeck4.csv"))
   expect_message(validate_deck(test_path("testdata", "testdeck4.csv"),
-                               package = FALSE
+    package = FALSE
   ), "so using filename for title")
-  suppressMessages(expect_error(validate_deck(
+  suppressMessages(expect_message(validate_deck(
     test_path("testdata", "testdeck4.csv"),
     package = TRUE
-  )))
+  ), "This deck does not include a package column"))
   testdeck5 <- testdeck[, 1]
   write.csv(testdeck5, test_path("testdata", "testdeck5.csv"))
   file.remove(test_path("testdata", "testdeck3.csv"))
   file.remove(test_path("testdata", "testdeck4.csv"))
   file.remove(test_path("testdata", "testdeck5.csv"))
   testflash <- validate_deck(test_path("testdata", "operators.csv"),
-                             package = TRUE
+    package = TRUE
   )
   expect_true("title" %in% names(testflash))
 })
