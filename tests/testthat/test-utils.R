2dd3f4dfcad27942b9af58150a796818fc3e1263
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2024-10-01 15:35:58 -0500

    Skip testing fail_gracefully() on CI

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,5 +1,6 @@
 test_that("connections to internet resources fail gracefully", {
   skip_on_cran()
+  skip_on_ci()
   suppressMessages(expect_message(fail_gracefully("http://httpbin.org/status/404"), "Not Found \\(HTTP 404\\)"))
   suppressMessages(expect_message(fail_gracefully("http://httpbin.org/delay/2", 1), "Timeout was reached"))
   expect_silent(fail_gracefully("http://httpbin.org/"))
